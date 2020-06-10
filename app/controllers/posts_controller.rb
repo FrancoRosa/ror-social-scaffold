3628ff7794d7a2dd6471e211b66cac5b5a4e46d8
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2020-06-10 20:48:20 +0100

    Update timeline posts to show only posts from friends

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,13 @@ class PostsController < ApplicationController
   private
 
   def timeline_posts
-    @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+    # @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
+    posts = []
+
+    posts += current_user.posts
+    current_user.friends.each{ |friend| posts += friend.posts }
+
+    @timeline_posts ||= posts.uniq.sort{ |a,b| a.created_at <=> b.created_at }.reverse
   end
 
   def post_params
