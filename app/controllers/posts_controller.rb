8d2277c7d83ec21dc67194c39d147516e6f2a01a
Zakariae El Mejdki <elmejdki.zakariae@gmail.com>
2020-06-10 20:51:23 +0100

    Remove unnecessary comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,6 @@ class PostsController < ApplicationController
   private
 
   def timeline_posts
-    # @timeline_posts ||= Post.all.ordered_by_most_recent.includes(:user)
     posts = []
 
     posts += current_user.posts
